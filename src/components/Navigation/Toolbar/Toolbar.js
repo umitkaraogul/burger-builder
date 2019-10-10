d21e08787b933d18a654cb92c70963bfcf8d9129
Umit Karaogul <umit.karaogul@freelancer.run>
2019-10-10 09:13:17 +0100

    navigation items added

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,14 +2,17 @@ import React from 'react';
 
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
-const toolbar=()=>(
-    <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo />
-        <nav>
-            ...
-        </nav>
-    </header>
+const toolbar = () => (
+  <header className={classes.Toolbar}>
+    <div>MENU</div>
+    <div className={classes.Logo}>
+      <Logo />
+    </div>
+    <nav>
+      <NavigationItems />
+    </nav>
+  </header >
 )
 export default toolbar;
