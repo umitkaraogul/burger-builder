b4908ea7ce06f866535309700eb93a16645740e2
Umit Karaogul <umit.karaogul@freelancer.run>
2019-10-21 13:00:13 +0100

    interceptor rejectted

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,7 +27,6 @@ class BurgerBuilder extends Component {
 
     }
     componentDidMount() {
-        console.log("componentDidMount BurgerBuilder");
         axios.get('/ingredients.json')
             .then(response => {
                 console.log(response);
